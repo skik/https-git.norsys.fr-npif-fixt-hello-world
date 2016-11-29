aaa78f7ab09a310614b30e0ca153476b738fbfaf
Michael BOURGEOIS <mbourgeois2@npif.local>
2016-11-29 13:23:48 +0100

    On est vraiment trop les meilleurs !

--- src/main/java/fr/norsys/sandbox/helloworld/App.java
+++ src/main/java/fr/norsys/sandbox/helloworld/App.java
@@ -7,5 +7,7 @@ package fr.norsys.sandbox.helloworld;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("o_O Je suis le meilleur !");
+		System.out.println("Le mec est trop fort !");
 	}
 }
